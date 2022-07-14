e45dda9e9076d5842e26fe0aad287d811bfeacd8
Kale Leach <kleach1212@gmail.com>
2022-07-13 19:58:07 -0700

    commit tree bb66a7ea95188cbc3537c40014e744445206ad57

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+  require 'pry'
 
   get '/games' do
     games = Game.all.order(:title).limit(10)
@@ -14,6 +15,36 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+  end
+
+  delete '/reviews/:id' do
+
+    review = Review.find(params[:id])
+
+    review.destroy
+
+    review.to_json
+
+  end
+
+  post '/reviews' do 
+    
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
   end
 
 end
